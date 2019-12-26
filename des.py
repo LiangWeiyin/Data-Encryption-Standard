bcd79241e67f49132db533b5b9ba3cadd1fb931d
LiangWeiyin <lwy135244@gmail.com>
2019-12-26 20:04:00 +0800

    modify

--- des.py
+++ des.py
@@ -34,10 +34,8 @@ def bits_to_string(data, type):
         res = ''.join(temp)
         return res
     elif type == 0:
-        while(data[-1] == '0' and data[-2] == '0'): #去掉补上的0
-            data.pop(-1)
-        if data[-1] == '0':
-            data.pop(-1)
+        while (''.join(data[-8:]) =='00000000'):
+            data = data[:-8]
         temp = [int(''.join(data[ind:ind+8]), 2) for ind  in range(0, len(data), 8)]
         res = bytes(temp).decode('utf-8')
         return res
@@ -46,11 +44,11 @@ def text_slice(text):
     # 明文分组
     return [ text[ind:ind+64] for ind in range(0, len(text), 64) ]
 
-def permutation(s, matrix):
+def permutation(data, matrix):
     # 矩阵置换
     res = []
     for val in matrix:
-        res.append(s[val-1])
+        res.append(data[val-1])
     return res
 
 def gen_key():
@@ -68,8 +66,7 @@ def gen_subkeys(key):
         temp_right = left_shift(right_block, KEY_SHIFT_AMOUNTS[i])
         sub_key = permutation(temp_left+temp_right, PERMUTED_CHOICE_2)
         sub_keys.append(sub_key)
-        left_block = temp_left
-        right_block = temp_right
+        left_block, right_block = temp_left, temp_right
     return sub_keys
 
 def left_shift(s, num):
@@ -86,13 +83,13 @@ def xor(s1, s2):
             res.append('1')
     return res
 
-def sbox_fuction(right_bolck, subkey):
+def F_fuction(right_bolck, subkey):
     # 轮函数, S-BOX变换
     res = []
     expand_right = permutation(right_bolck, EXPANSION)
     right_xor_key = xor(expand_right, subkey)
-    text_for_sbox = [right_xor_key[ind:ind+6] for ind in range(0, len(right_xor_key), 6)]
-    for ind, val in enumerate(text_for_sbox):
+    data_for_sbox = [right_xor_key[ind:ind+6] for ind in range(0, len(right_xor_key), 6)]
+    for ind, val in enumerate(data_for_sbox):
         s_row = int(''.join([val[0], val[-1]]), 2)
         s_column = int(''.join(val[1:-1]), 2)
         temp = list(bin(SBOXES[ind][s_row][s_column])[2:].zfill(4))
@@ -104,7 +101,7 @@ def round(subkeys, block):
     left_block, right_block = block[:32], block[32:]
     for ind in range(16):
         temp = right_block
-        sbox_out = sbox_fuction(right_block, subkeys[ind])
+        sbox_out = F_fuction(right_block, subkeys[ind])
         right_block = xor(left_block, sbox_out)
         left_block = temp
     return right_block + left_block
@@ -114,7 +111,8 @@ def encryption(text, key, mod='ECB', iv=''):
     res = []
     bits = string_to_bits(text, type=1)
     group_text = text_slice(bits)
-    subkeys = gen_subkeys(key)
+    k = string_to_bits(key, type=1)
+    subkeys = gen_subkeys(k)
     if mod == 'ECB':
         for block in group_text:
             permu_text = permutation(block, INITIAL_PERMUTATION)
@@ -124,13 +122,14 @@ def encryption(text, key, mod='ECB', iv=''):
         ciphertext = bits_to_string(res, type=1)
         return ciphertext
     elif mod == 'CBC':
+        IV = string_to_bits(iv, type=1)
         for block in group_text:
-            xor_block = xor(block, iv)
+            xor_block = xor(block, IV)
             permu_text = permutation(xor_block, INITIAL_PERMUTATION)
             round_end = round(subkeys, permu_text)
             final_permu_text = permutation(round_end, FINAL_PERMUTATION)
             res.extend(final_permu_text)
-            iv = final_permu_text
+            IV = final_permu_text
         ciphertext = bits_to_string(res, type=1)
         return ciphertext
 
@@ -139,7 +138,8 @@ def decryption(text, key, mod='ECB', iv=''):
     res = []
     bits = string_to_bits(text, type=0)
     group_text = text_slice(bits)
-    subkeys = gen_subkeys(key)
+    k = string_to_bits(key, type=1)
+    subkeys = gen_subkeys(k)
     subkeys.reverse()  #调换子密钥顺序
     if mod == 'ECB':
         for block in group_text:
@@ -150,12 +150,13 @@ def decryption(text, key, mod='ECB', iv=''):
         plaintext = bits_to_string(res, type=0)
         return plaintext
     if mod == 'CBC':
+        IV = string_to_bits(iv, type=1)
         for block in group_text:
             permu_text = permutation(block, INITIAL_PERMUTATION)
             round_end = round(subkeys, permu_text)
             final_permu_text = permutation(round_end, FINAL_PERMUTATION)
-            xor_block = xor(final_permu_text, iv)
+            xor_block = xor(final_permu_text, IV)
             res.extend(xor_block)
-            iv = block
+            IV = block
         plaintext = bits_to_string(res, type=0)
         return plaintext
--- run.py
+++ run.py
@@ -1,24 +1,23 @@
 import des
 
 if __name__ == '__main__':
-    mes = "Hello World!"
-    IV = list('0110000101100010011000110110010000110001001100100011001100110100')    #abcd1234
-    key1 = list('0110000101100010011000110110010000110001001100100011001100110100')  #abcd1234
-    key2 = list('0110000101100010011000110110010000110001001100100011001100110100')  #abcd1234
+    mes = "abcd1234abcd1234"
+    IV = 'abcd1234'    #初始向量
+    key = 'abcd1234'   #密钥
     print('----------------------------------------------------------------------')
     print('----------------------------------------------------------------------')
     print('消息: {}'.format(mes))
     print('----------------------------------------------------------------------')
     print('----------------------------------------------------------------------')
-    ciphertext = des.encryption(mes, key1)
-    # ciphertext = des.encryption(mes, key1, mod='CBC', iv=IV)
+    ciphertext = des.encryption(mes, key)
+    # ciphertext = des.encryption(mes, key, mod='CBC', iv=IV)
     with open('ciphertext.txt', 'w+', encoding='utf-8') as f:
         f.write('{}\n'.format(ciphertext))
     print('加密: {}'.format(ciphertext))
     print('----------------------------------------------------------------------')
     print('----------------------------------------------------------------------')
-    plaintext = des.decryption(ciphertext, key2)
-    # plaintext = des.decryption(ciphertext, key2, mod='CBC', iv=IV)
+    plaintext = des.decryption(ciphertext, key)
+    # plaintext = des.decryption(ciphertext, key, mod='CBC', iv=IV)
     with open('plaintext.txt', 'w+') as f:
         f.write('{}\n'.format(plaintext))
     print('解密: {}'.format(plaintext))
